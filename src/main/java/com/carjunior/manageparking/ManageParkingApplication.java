1435045dd9c051acc430b5eef0879de08aa0b7d9
car-junior <carlosilva.dev@gmail.com>
2024-03-25 17:33:33 -0300

    Criado Projeto para Gerenciamento de Estacionamentos

---
+++ src/main/java/com/carjunior/manageparking/ManageParkingApplication.java
@@ -0,0 +1,13 @@
+package com.carjunior.manageparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageParkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageParkingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carjunior/manageparking/ManageParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carjunior.manageparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageParkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
