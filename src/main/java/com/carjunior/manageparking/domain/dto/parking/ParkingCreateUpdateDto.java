de83b298dcd1241fb62672bbefedc44f906fbfd8
car-junior <carlosilva.dev@gmail.com>
2024-04-06 23:53:34 -0300

    feat: Adiciona permissões e roles na criação de usuário

--- src/main/java/com/carjunior/manageparking/domain/dto/parking/ParkingCreateUpdateDto.java
+++ src/main/java/com/carjunior/manageparking/domain/dto/parking/ParkingCreateUpdateDto.java
@@ -1,11 +1,12 @@
 package com.carjunior.manageparking.domain.dto.parking;
 
+import com.carjunior.manageparking.domain.entity.enums.Permission;
+import com.carjunior.manageparking.domain.entity.enums.RoleType;
 import com.carjunior.manageparking.domain.validators.phone.PhoneBR;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.*;
 import org.hibernate.validator.constraints.br.CNPJ;
+
+import java.util.Set;
 
 import static com.carjunior.manageparking.domain.utils.Utility.onlyNumbers;
 
@@ -15,7 +16,9 @@ public record ParkingCreateUpdateDto(
         @NotNull @NotEmpty String address,
         @NotNull @NotEmpty @PhoneBR String phoneNumber,
         @NotNull @Positive @Min(10) Integer numberSpacesMotorcycles,
-        @NotNull @Positive @Min(10) Integer numberSpacesCars
+        @NotNull @Positive @Min(10) Integer numberSpacesCars,
+        @NotNull @Size(min = 1) Set<RoleType> roles,
+        @NotNull @Size(min = 1) Set<Permission> permissions
 ) {
     public ParkingCreateUpdateDto {
         if (name != null)
