c758009172bce5e729e04e126effe45069b1acbe
car-junior <carlosilva.dev@gmail.com>
2024-04-10 23:56:07 -0300

    feat: Adiciona serviço para gerenciar token jwt; Adiciona extração de informações do token

---
+++ src/main/java/com/carjunior/manageparking/domain/service/authentication/JwtService.java
@@ -0,0 +1,38 @@
+package com.carjunior.manageparking.domain.service.authentication;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+    private final String MOMENT_SECRET = "1236456ASDZXC";
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(MOMENT_SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
