566847ca4488538c944246865a33714a926d080a
car-junior <carlosilva.dev@gmail.com>
2024-04-10 23:58:45 -0300

    feat: Adiciona método que cria o token

--- src/main/java/com/carjunior/manageparking/domain/service/authentication/JwtService.java
+++ src/main/java/com/carjunior/manageparking/domain/service/authentication/JwtService.java
@@ -2,12 +2,15 @@ package com.carjunior.manageparking.domain.service.authentication;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -29,6 +32,15 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private String createToken(Map<String, Object> claims, String username) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+ 1000 * 60))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
     }
 
     private Key getSignKey() {
