9377815590c8f1b0a9dce7a2d97a67bc4bed0779
car-junior <carlosilva.dev@gmail.com>
2024-04-15 00:00:08 -0300

    feat: Cria classe customizada para UserDetails

--- src/main/java/com/carjunior/manageparking/domain/entity/security/CustomUserDetails.java
+++ src/main/java/com/carjunior/manageparking/domain/entity/security/CustomUserDetails.java
@@ -6,6 +6,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 
 public class CustomUserDetails implements UserDetails {
+
+
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
